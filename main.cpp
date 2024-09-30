beb5adacb5fdc23cf149af0f7070bbaf8fa27c97
iiokaisami <k023g0025@g.neec.ac.jp>
2024-09-30 11:46:35 +0900

    00_01完成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <Windows.h>
 
 int main(){
 
+	//SetConsoleOutputCP(65001);
+	char str[] = "あいうえお";
+	printf("%s\n", str);
+
+	printf("Hallow World!\n");
+	printf("ハローワールド\n");
 
 	return 0;
 }
