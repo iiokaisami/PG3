7847ea344430b0499dd5c9930d92f3365a630658
iiokaisami <k023g0025@g.neec.ac.jp>
2024-10-07 12:13:37 +0900

    01_02完成

--- main.cpp
+++ main.cpp
@@ -1,13 +1,35 @@
 #include <stdio.h>
-#include <Windows.h>
+
+template <typename Type>
+Type General(Type money, Type time) {
+
+	return money * time;
+}
+
+template <typename Type>
+Type Recursive(Type money, Type time) {
+	
+	auto result = 0;
+
+	if (time <= 1) {
+		return money;
+	}
+
+	result = Recursive(money * 2 - 50, time - 1);
+
+	return result;
+}
+
 
 int main(){
 
-	//SetConsoleOutputCP(65001);
-	char str[] = "あいうえお";
-	printf("%s\n", str);
-	printf("Hallow World!\n");
-	printf("ハローワールド\n");
+	int recursiveMoney = 100;
+	int generalMoney = 1072;
+	int time = 4;
+	int getRecursiveMoney = Recursive(recursiveMoney, time);
+	int getGeneralMoney = General(generalMoney, time);
+
+	printf("%d時間働いたとき、一般的な資金体系だと %d 円、再帰的な資金体系だと %d 円\n", time, getGeneralMoney, getRecursiveMoney);
 
 	return 0;
 }
