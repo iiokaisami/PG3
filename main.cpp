8ce06e1981c0fece2d0a4b7c53d848b53917f93f
iiokaisami <k023g0025@g.neec.ac.jp>
2024-10-07 12:29:19 +0900

    01_02修正

--- main.cpp
+++ main.cpp
@@ -11,8 +11,12 @@ Type Recursive(Type money, Type time) {
 	
 	auto result = 0;
 
-	if (time <= 1) {
+	if (time == 1) {
 		return money;
+	}
+	else if (time < 1)
+	{
+		return result;
 	}
 
 	result = Recursive(money * 2 - 50, time - 1);
@@ -25,11 +29,19 @@ int main(){
 
 	int recursiveMoney = 100;
 	int generalMoney = 1072;
-	int time = 4;
-	int getRecursiveMoney = Recursive(recursiveMoney, time);
-	int getGeneralMoney = General(generalMoney, time);
+	const int time = 11;
+	int getRecursiveMoney = 0;
 
-	printf("%d時間働いたとき、一般的な資金体系だと %d 円、再帰的な資金体系だと %d 円\n", time, getGeneralMoney, getRecursiveMoney);
+	for (int i = 0; i < time; i++)
+	{
+		int hourRecursiveMoney = Recursive(recursiveMoney, i);
+		int getGeneralMoney = General(generalMoney, i);
+
+		getRecursiveMoney += hourRecursiveMoney;
+
+		printf("%d時間働いたとき、一般的な資金体系だと %d 円、再帰的な資金体系だと %d 円\n\n", i, getGeneralMoney, getRecursiveMoney);
+	}
+	
 
 	return 0;
 }
