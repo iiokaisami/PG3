96ede7cea62d81575eff56d5622abc3661cc41d1
iiokaisami <k023g0025@g.neec.ac.jp>
2024-10-28 12:28:25 +0900

    02_02完成

---
+++ Comparing.cpp
@@ -0,0 +1,1 @@
+#include "Comparing.h"
--- main.cpp
+++ main.cpp
@@ -1,16 +1,30 @@
 #include <stdio.h>
 #include <Windows.h>
 
-#include "Enemy.h"
+#include "Comparing.h"
 
 int main() {
 
-    Enemy* enemy = new Enemy;
-   
-    enemy->Update();
+    int numInt1 = 99;
+    int numInt2 = 100;
 
+    float numFloat1 = 99.9f;
+    float numFloat2 = 10.1f;
+
+    double numDouble1 = 10.1f;
+    double numDouble2 =  9.9f;
+
+    Comparing<int> resultInt(numInt1, numInt2);
+    int numInt = resultInt.Min(resultInt.num1, resultInt.num2);
+    printf("%d\n", numInt);
+
+    Comparing<float> resultFloat(numFloat1, numFloat2);
+    float numFloat = resultFloat.Min(resultFloat.num1, resultFloat.num2);
+    printf("%f\n", numFloat);
+
+    Comparing<double> resultDouble(numDouble1, numDouble2);
+    double numDouble = resultDouble.Min(resultDouble.num1, resultDouble.num2);
+    printf("%f\n", numDouble);
 
     return 0;
-
-    delete enemy;
 }
