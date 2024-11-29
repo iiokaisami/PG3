82a65a5525fcaa98aad424fda5dafb2768591e98
iiokaisami <k023g0025@g.neec.ac.jp>
2024-11-29 12:48:57 +0900

    03_02完成

--- main.cpp
+++ main.cpp
@@ -1,50 +1,52 @@
-#include <Windows.h>
-#include <iostream>
-#include <list>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cassert>
+#include <sstream>
+#include <fstream>
 
-using namespace std;
+std::vector<std::string> mailAddress;
 
-void Draw(const list<const char*>& stations) {
-    for (const auto& station : stations) {
-       
-        printf(" %s ,", station);
-    }  
+void LoadData()
+{
+    // ファイルを読み込む
+    std::ifstream file("PG3_2024_03_02.txt");
+    std::string line;
+
+    // ファイルの内容を格納
+    if (file.is_open())
+    {
+        while (std::getline(file, line)) 
+        {
+            std::stringstream line_stream(line);
+            std::string email;
+            // ,で区切る
+            while (std::getline(line_stream, email, ','))
+            {
+                mailAddress.push_back(email);
+            }
+        }
+        file.close();
+    }
+    else
+    {
+        printf("\nファイルを開けませんでした\n");
+    }
+
 }
 
 int main() {
 
-    // 駅名
-    list<const char*> stations1970 = {
-        "Tokyo", "Kanda", "Akihabara", "Okachimachi", "Ueno", "Uguisudani",
-        "Nippori", "Tabata", "Komagome", "Sugamo", "Otsuka", "Ikebukuro",
-        "Mejiro", "Takadanobaba", "Shin-Okubo", "Shinjuku", "Yoyogi", "Harajuku",
-        "Shibuya", "Ebisu", "Meguro", "Gotanda", "Osaki", "Shinagawa",
-        "Tamachi", "Hamamatsucho", "Shimbashi", "Yurakucho"
-    };
+    LoadData();
+   
+    // 番号順にソート
+    std::sort(mailAddress.begin(), mailAddress.end());
 
-    // 1971年西日暮里駅を追加
-    auto it = stations1970.begin();
-    advance(it, 8);
-    stations1970.insert(it, "Nishi-Nippori");
-
-    // 2019年西日暮里駅を追加
-    list<const char*> stations2019 = stations1970;
-
-    // 2022年高輪ゲートウェイ駅を追加
-    list<const char*> stations2022 = stations2019;
-    it = stations2022.begin();
-    advance(it, 22);
-    stations2022.insert(it, "Takanawa Gateway");
-
-    // 表示
-    printf("\n\nStations in 1970\n");
-    Draw(stations1970);
-
-    printf("\n\nStations in 2019\n");
-    Draw(stations2019);
-
-    printf("\n\nStations in 2022\n");
-    Draw(stations2022);
+    // 番号順に並べて表示
+    for (const auto& email : mailAddress)
+    {
+        printf("%s\n", email.c_str());
+    }
 
     return 0;
 }
